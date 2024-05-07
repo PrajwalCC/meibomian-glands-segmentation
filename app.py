6d5cea49e07f53bca6afa5ffb2c774e66e97a5b3
PrajwalCC <b220035@iiit-bh.ac.in>
2024-05-07 16:15:46 +0530

    eye lid segmentation added

--- app.py
+++ app.py
@@ -7,41 +7,61 @@ import cv2
 
 app = Flask(__name__)
 
-# Load pre-trained model
-model = load_model('models/gland-segmentation.h5')
+# Load pre-trained models
+glandModel = load_model('models/gland-segmentation.h5')
+eyeLidModel = load_model('models/eyelid-segmentation.h5')
 
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/predict/gland', methods=['POST'])
+def predict_gland():
     # Get image file from POST request
     image_file = request.files['image']
-    # print(request.files['hello'])
 
     # Read and preprocess image
     image = preprocess_image(image_file)
 
-    # Make predictions
-    predictions = model.predict(np.expand_dims(image, axis=0))
-    
-    # print(predictions.dtype)
-    # print(predictions.shape)
+    # Make gland segmentation prediction
+    gland_predictions = glandModel.predict(np.expand_dims(image, axis=0))
+    gland_predictions_flat = gland_predictions[0, :, :, 0]
 
-    predictions_flat = predictions[0, :, :, 0]
-
-# Convert to uint8 and scale to [0, 255]
-    predictions_uint8 = (predictions_flat * 255).astype(np.uint8)
+    # Convert to uint8 and scale to [0, 255]
+    gland_predictions_uint8 = (gland_predictions_flat * 255).astype(np.uint8)
 
     # Create PIL image
-    predicted_output_image = Image.fromarray(predictions_uint8)
-
-
+    gland_output_image = Image.fromarray(gland_predictions_uint8)
 
     # Save the predicted output image to a BytesIO object
-    img_byte_array = BytesIO()
-    predicted_output_image.save(img_byte_array, format='PNG')
-    img_byte_array.seek(0)
+    gland_img_byte_array = BytesIO()
+    gland_output_image.save(gland_img_byte_array, format='PNG')
+    gland_img_byte_array.seek(0)
 
-    # Return the image as response
-    return send_file(img_byte_array, mimetype='image/png')
+    # Return the gland image as response
+    return send_file(gland_img_byte_array, mimetype='image/png')
+
+@app.route('/predict/eyelid', methods=['POST'])
+def predict_eyelid():
+    # Get image file from POST request
+    image_file = request.files['image']
+
+    # Read and preprocess image
+    image = preprocess_image(image_file)
+
+    # Make eyelid segmentation prediction
+    eyelid_predictions = eyeLidModel.predict(np.expand_dims(image, axis=0))
+    eyelid_predictions_flat = eyelid_predictions[0, :, :, 0]
+
+    # Convert to uint8 and scale to [0, 255]
+    eyelid_predictions_uint8 = (eyelid_predictions_flat * 255).astype(np.uint8)
+
+    # Create PIL image
+    eyelid_output_image = Image.fromarray(eyelid_predictions_uint8)
+
+    # Save the predicted output image to a BytesIO object
+    eyelid_img_byte_array = BytesIO()
+    eyelid_output_image.save(eyelid_img_byte_array, format='PNG')
+    eyelid_img_byte_array.seek(0)
+
+    # Return the eyelid image as response
+    return send_file(eyelid_img_byte_array, mimetype='image/png')
 
 def preprocess_image(image_file):
     # Read image file and convert to numpy array
